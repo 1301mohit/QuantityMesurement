d3109b95403e00c64ecdc37fd5ced93cac4ad8ad
1301mohit <mohitkr1301@gmail.com>
2019-12-17 14:18:27 +0530

    We define Length entity in place of Feet and Inch entity

--- src/test/java/quantitymeasurement/quantityTest.java
+++ src/test/java/quantitymeasurement/quantityTest.java
@@ -2,43 +2,43 @@ package quantitymeasurement;
 
 import org.junit.Assert;
 import org.junit.Test;
-import quantitymesurement.Feet;
+import quantitymesurement.Length;
 
 public class quantityTest {
 
     @Test
     public void given0Feetand0Feet_ShouldReturnEqual(){
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(0.0);
-        Assert.assertEquals(feet1, feet2);
+        Length length1 = new Length(Length.Unit.FEET,0.0);
+        Length length2 = new Length(Length.Unit.FEET,0.0);
+        Assert.assertEquals(length1, length2);
     }
 
     @Test
     public void given0Feetand1Feet_ShouldReturnNotEqual(){
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(1.0);
+        Length feet1 = new Length(Length.Unit.FEET,0.0);
+        Length feet2 = new Length(Length.Unit.FEET,1.0);
         Assert.assertNotEquals(feet1, feet2);
     }
 
     @Test
     public void given0Feetand1Feet_ShouldReturnFalse(){
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(1.0);
+        Length feet1 = new Length(Length.Unit.FEET,0.0);
+        Length feet2 = new Length(Length.Unit.FEET,1.0);
         boolean equals = feet1.equals(feet2);
         Assert.assertFalse(equals);
     }
 
     @Test
     public void given0Inchand0Inch_ShouldReturnEqual(){
-        Inch inch1 = new Inch(0.0);
-        Inch inch2 = new Inch(0.0);
+        Length inch1 = new Length(Length.Unit.INCH,0.0);
+        Length inch2 = new Length(Length.Unit.INCH,0.0);
         Assert.assertEquals(inch1, inch2);
     }
 
     @Test
     public void given0Inchand1Inch_ShouldReturnNotEqual(){
-        Inch inch1 = new Inch(0.0);
-        Inch inch2 = new Inch(1.0);
+        Length inch1 = new Length(Length.Unit.INCH,0.0);
+        Length inch2 = new Length(Length.Unit.INCH,1.0);
         Assert.assertNotEquals(inch1, inch2);
     }
 
