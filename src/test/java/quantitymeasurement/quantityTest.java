e50ced1133685ee201fd851c38b70bbfaab79f91
1301mohit <mohitkr1301@gmail.com>
2019-12-17 13:54:37 +0530

    code and test cases for quantity mesurement

---
+++ src/test/java/quantitymeasurement/quantityTest.java
@@ -0,0 +1,45 @@
+package quantitymeasurement;
+
+import org.junit.Assert;
+import org.junit.Test;
+import quantitymesurement.Feet;
+
+public class quantityTest {
+
+    @Test
+    public void given0Feetand0Feet_ShouldReturnEqual(){
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assert.assertEquals(feet1, feet2);
+    }
+
+    @Test
+    public void given0Feetand1Feet_ShouldReturnNotEqual(){
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotEquals(feet1, feet2);
+    }
+
+    @Test
+    public void given0Feetand1Feet_ShouldReturnFalse(){
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        boolean equals = feet1.equals(feet2);
+        Assert.assertFalse(equals);
+    }
+
+    @Test
+    public void given0Inchand0Inch_ShouldReturnEqual(){
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(0.0);
+        Assert.assertEquals(inch1, inch2);
+    }
+
+    @Test
+    public void given0Inchand1Inch_ShouldReturnNotEqual(){
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        Assert.assertNotEquals(inch1, inch2);
+    }
+
+}
